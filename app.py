c6c94f8ae2d99b420c9e6324c02eb38b338ea365
Vansh Patel <patelvansh2009@proton.me>
2026-07-22 21:06:57 +0100

    Added simple login/signup pages to homepage, and linked them via flask

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+
+@app.route("/generate", methods=["POST"])
+def generate():
+    print("Button clicked!")
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
